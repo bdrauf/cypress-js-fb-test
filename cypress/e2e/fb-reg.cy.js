9fa563be4a383db3c028b3316f83c2b2e43923f6
Abd. Rauf <bdsurel19@gmail.com>
2023-06-20 15:39:50 +0700

    add invalid email format testcase

--- cypress/e2e/fb-reg.cy.js
+++ cypress/e2e/fb-reg.cy.js
@@ -1,5 +1,6 @@
 describe('fb reg', () => {
-    it('test1', () => {
+    it('reg-positive', () => {
+
         cy.visit("https://id-id.facebook.com/")
 
         cy.get(".fb_logo").click()
@@ -28,14 +29,43 @@ describe('fb reg', () => {
 
         cy.get(".x6s0dn4 > .x193iq5w > .x1lliihq").should("be.visible")
 
-        let expName="FbTest"
+        let expName = "FbTest"
 
         cy.get(".x6s0dn4 > .x193iq5w > .x1lliihq").then( (x) =>{
-            let actName=x.text()
+            let actName = x.text()
 
-            expect(actName).to.equal(expName)
+            assert.equal(actName,expName)
 
             })
+    })
+
+    it('invalid-email-negative', () =>{
+
+        cy.visit("https://id-id.facebook.com/")
+
+        cy.get(".fb_logo").click()
+
+        cy.get("[data-testid='open-registration-form-button']").click()
+
+        cy.get("[aria-label='Nama depan']").type("FbTest")
+
+        cy.get("[aria-label='Nama belakang']").type("Reg")
+
+        cy.get("[name='reg_email__']").type("salah")
+
+        cy.get("[name='reg_passwd__']").type("FbTest123!")
+        
+        cy.get("#day").select("21")
+
+        cy.get("#month").select("Jun")
+
+        cy.get("#year").select("1999")
+
+        cy.get("[type='radio']").check("2")
+
+        cy.get("[name='websubmit']").click()
+
+        cy.get("._5v-0._53im").contains("Harap masukkan nomor telepon atau alamat email yang valid.").should("be.visible")
 
     })
 })
